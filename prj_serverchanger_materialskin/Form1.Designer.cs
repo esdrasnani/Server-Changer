6511fc0b2205615d40055aa0ab622bf3d63932aa
Esdras Nani <esdraslnani@hotmail.com>
2020-07-20 19:32:47 -0300

    Adicionar arquivos de projeto.

---
+++ prj_serverchanger_materialskin/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace prj_serverchanger_materialskin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialListView1 = new MaterialSkin.Controls.MaterialListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.materialProgressBar1 = new MaterialSkin.Controls.MaterialProgressBar();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lbl_pct = new MaterialSkin.Controls.MaterialLabel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialListView1
+            // 
+            this.materialListView1.AllowDrop = true;
+            this.materialListView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialListView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.materialListView1.Depth = 0;
+            this.materialListView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.materialListView1.ForeColor = System.Drawing.Color.Black;
+            this.materialListView1.FullRowSelect = true;
+            this.materialListView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.materialListView1.HideSelection = false;
+            this.materialListView1.Location = new System.Drawing.Point(12, 94);
+            this.materialListView1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialListView1.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialListView1.Name = "materialListView1";
+            this.materialListView1.OwnerDraw = true;
+            this.materialListView1.Size = new System.Drawing.Size(827, 131);
+            this.materialListView1.TabIndex = 0;
+            this.materialListView1.UseCompatibleStateImageBehavior = false;
+            this.materialListView1.View = System.Windows.Forms.View.Details;
+            this.materialListView1.SelectedIndexChanged += new System.EventHandler(this.materialListView1_SelectedIndexChanged);
+            this.materialListView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.materialListView1_DragDrop);
+            this.materialListView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.materialListView1_DragEnter);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Local do Arquivo";
+            this.columnHeader1.Width = 776;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Server Changer";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(94, 26);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // materialProgressBar1
+            // 
+            this.materialProgressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialProgressBar1.Depth = 0;
+            this.materialProgressBar1.Location = new System.Drawing.Point(16, 247);
+            this.materialProgressBar1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialProgressBar1.Name = "materialProgressBar1";
+            this.materialProgressBar1.Size = new System.Drawing.Size(727, 5);
+            this.materialProgressBar1.TabIndex = 2;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(12, 72);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(123, 19);
+            this.materialLabel1.TabIndex = 4;
+            this.materialLabel1.Text = "Local do Arquivo";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // lbl_pct
+            // 
+            this.lbl_pct.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_pct.AutoSize = true;
+            this.lbl_pct.Depth = 0;
+            this.lbl_pct.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lbl_pct.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_pct.Location = new System.Drawing.Point(749, 242);
+            this.lbl_pct.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lbl_pct.Name = "lbl_pct";
+            this.lbl_pct.Size = new System.Drawing.Size(0, 19);
+            this.lbl_pct.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 279);
+            this.Controls.Add(this.lbl_pct);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialProgressBar1);
+            this.Controls.Add(this.materialListView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server Changer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialListView materialListView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private MaterialSkin.Controls.MaterialProgressBar materialProgressBar1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private MaterialSkin.Controls.MaterialLabel lbl_pct;
+    }
+}
+
---
+++ prj_serverchanger_materialskin/Form1.cs
@@ -0,0 +1,491 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+using MaterialSkin;
+using Ionic.Zip;
+using System.Threading;
+
+namespace prj_serverchanger_materialskin
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        MaterialSkinManager skinManager;
+        int _TotalArquivos = 0;
+        int _count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.DARK;
+            skinManager.ColorScheme = new ColorScheme(Primary.Blue600, Primary.Blue900, Primary.Blue900, Accent.LightBlue200, TextShade.WHITE);
+
+            materialListView1.AllowDrop = true;
+
+            backgroundWorker1.WorkerReportsProgress = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            materialListView1.Columns[0].Width = materialListView1.Width - 4;
+        }
+
+        private void materialListView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void materialListView1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            for (int i = 0; i < s.Length; i++)
+            {
+                FileInfo fi = new FileInfo(s[i]);
+                if (fi.Extension == ".twb" || fi.Extension == ".twbx")
+                    materialListView1.Items.Add(s[i]);
+                else
+                {
+                    MessageBox.Show("Arquivo " + s[i] + " Inválido!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if (s.Length == 1)
+                        return;
+                }
+            }
+
+
+            int count = 0;
+            List<String> files_path = new List<String>();
+            for (int i = 0; i < materialListView1.Items.Count; i++)
+            {
+                files_path.Add(materialListView1.Items[i].Text);
+            }
+
+            _TotalArquivos = materialListView1.Items.Count;
+
+            materialProgressBar1.Maximum = _TotalArquivos;
+            lbl_pct.Text = "";
+
+            BackgroundWorker bgw;
+            for (int i = 0; i < materialListView1.Items.Count; i++)
+            {
+                List<Object> arguments = new List<object>();
+                arguments.Add(files_path);
+                arguments.Add(i);
+
+                bgw = new BackgroundWorker();
+                bgw.DoWork += backgroundWorker1_DoWork;
+                bgw.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+                bgw.ProgressChanged += backgroundWorker1_ProgressChanged;                
+                bgw.RunWorkerAsync(arguments);
+            }
+        }
+
+        #region Botão Mudar Servidor
+
+        private void btn_mudar_servidor_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            List<String> files_path = new List<String>();
+            for (int i = 0; i < materialListView1.Items.Count; i++)
+            {
+                files_path.Add(materialListView1.Items[i].Text);
+            }
+
+
+
+            for (int i = 0; i < materialListView1.Items.Count; i++)
+            {
+                FileInfo fi = new FileInfo(files_path[i]);
+
+                if (fi.Extension == ".twb")
+                {
+                    materialProgressBar1.Minimum = 0;
+                    materialProgressBar1.Maximum = 7;
+                    materialProgressBar1.Step = 1;
+                    int pct = (materialProgressBar1.Value / 22) * 100;
+
+                    File.Move(files_path[i], Path.ChangeExtension(files_path[i], ".xml"));
+                    materialProgressBar1.PerformStep();
+                    ;
+
+                    files_path[i] = Path.ChangeExtension(files_path[i], ".xml");
+                    materialProgressBar1.PerformStep();
+                    ;
+
+                    XDocument doc;
+                    try
+                    {
+                        doc = XDocument.Load(files_path[i]);
+                        materialProgressBar1.PerformStep();
+
+
+                    }
+                    catch (XmlException xmle)
+                    {
+                        MessageBox.Show("Erro ao Carregar o Arquivo!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show(xmle.ToString());
+                        return;
+                    }
+
+                    var elements = doc.Descendants("connection");
+                    materialProgressBar1.PerformStep();
+                    ;
+
+                    foreach (XElement element in elements)
+                    {
+                        if ((string)element.Attribute("server") != null || element.Attribute("server").ToString() != "")
+                        {
+                            element.Attribute("server").Value = "tableau.scanntech.com";
+                            materialProgressBar1.PerformStep();
+                            ;
+
+                        }
+                    }
+
+                    doc.Save(files_path[i]);
+                    materialProgressBar1.PerformStep();
+                    ;
+
+                    File.Move(files_path[i], Path.ChangeExtension(files_path[i], ".twb"));
+                    materialProgressBar1.PerformStep();
+                    ;
+
+                }
+                else if (fi.Extension == ".twbx")
+                {
+                    materialProgressBar1.Minimum = 0;
+                    materialProgressBar1.Maximum = 100;
+                    materialProgressBar1.Step = 1;
+
+                    int pct = (materialProgressBar1.Value / 22) * 100;
+                    File.Move(files_path[i], Path.ChangeExtension(files_path[i], ".zip"));
+                    materialProgressBar1.Value += 1;
+
+                    files_path[i] = Path.ChangeExtension(files_path[i], ".zip");
+                    materialProgressBar1.Value += 1;
+
+
+
+
+
+                    string file_twb = files_path[i].Replace("zip", "twb");
+                    int index = files_path[i].LastIndexOf(@"\");
+                    index++;
+                    file_twb = file_twb.Substring(index, file_twb.Length - index);
+
+                    string file_twb_aux;
+                    string path = files_path[i].Substring(0, index);
+                    using (ZipFile zip = ZipFile.Read(files_path[i]))
+                    {
+                        ZipEntry file = new ZipEntry();
+
+                        foreach (ZipEntry entry in zip.Entries)
+                        {
+                            if (entry.FileName.Contains("twb"))
+                            {
+                                file = entry;
+                                file.Extract(path);
+                                break;
+                            }
+                        }
+                        materialProgressBar1.Value += 1;
+
+
+
+                        zip.RemoveEntry(file);
+                        materialProgressBar1.Value += 1;
+
+
+
+                        zip.Save();
+                        materialProgressBar1.Value += 1;
+
+                        file_twb_aux = path + file.FileName;
+
+                        File.Move(file_twb_aux, Path.ChangeExtension(file_twb_aux, ".xml"));
+                        materialProgressBar1.Value += 1;
+                        file_twb_aux = Path.ChangeExtension(file_twb_aux, ".xml");
+                        materialProgressBar1.Value += 1;
+
+                    }
+
+                    XDocument doc;
+                    try
+                    {
+                        doc = XDocument.Load(file_twb_aux);
+                        materialProgressBar1.Value += 1;
+
+
+
+                    }
+                    catch (XmlException xmle)
+                    {
+                        MessageBox.Show("Erro ao Carregar o Arquivo!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show(xmle.ToString());
+                        return;
+                    }
+
+                    var elements = doc.Descendants("connection");
+
+                    foreach (XElement element in elements)
+                    {
+                        if ((string)element.Attribute("server") != null || element.Attribute("server").ToString() != "")
+                        {
+                            element.Attribute("server").Value = "tableau.scanntech.com";
+                            materialProgressBar1.Value += 1;
+                        }
+                    }
+
+                    doc.Save(file_twb_aux);
+
+                    materialProgressBar1.Value += 1;
+
+
+                    File.Move(file_twb_aux, Path.ChangeExtension(file_twb_aux, ".twb"));
+                    materialProgressBar1.Value += 1;
+
+
+
+                    file_twb_aux = Path.ChangeExtension(file_twb_aux, ".twb");
+
+
+                    using (ZipFile zip = ZipFile.Read(files_path[i]))
+                    {
+                        zip.AddFile(file_twb_aux, "");
+                        materialProgressBar1.Value += 1;
+
+
+
+                        zip.Save();
+                        materialProgressBar1.Value += 1;
+
+
+
+                    }
+
+                    materialProgressBar1.Value += 1;
+
+
+
+                    File.Delete(file_twb_aux);
+                    materialProgressBar1.Value += 1;
+
+
+
+                    File.Move(files_path[i], Path.ChangeExtension(files_path[i], ".twbx"));
+                    materialProgressBar1.Value += 1;
+
+
+
+                }
+                //count++;
+            }
+            MessageBox.Show("Servidor Alterado com Sucesso!", "Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            materialListView1.Items.Clear();
+            materialProgressBar1.Value = 0;
+        }
+        #endregion
+
+        private void materialListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            List<Object> arguments = e.Argument as List<object>;
+            List<String> files_path = (List<String>)arguments[0];
+            int i = (int)arguments[1];
+
+            FileInfo fi = new FileInfo(files_path[i]);
+
+            if (fi.Extension == ".twb")
+            {
+                File.Move(files_path[i], Path.ChangeExtension(files_path[i], ".xml"));
+
+
+                files_path[i] = Path.ChangeExtension(files_path[i], ".xml");
+
+
+                XDocument doc;
+                try
+                {
+                    doc = XDocument.Load(files_path[i]);
+
+
+
+                }
+                catch (XmlException xmle)
+                {
+                    MessageBox.Show("Erro ao Carregar o Arquivo!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show(xmle.ToString());
+                    return;
+                }
+
+                var elements = doc.Descendants("connection");
+
+
+                foreach (XElement element in elements)
+                {
+                    if ((string)element.Attribute("server") != null || element.Attribute("server").ToString() != "")
+                    {
+                        element.Attribute("server").Value = "tableau.scanntech.com";
+                    }
+                }
+
+                doc.Save(files_path[i]);
+
+
+                File.Move(files_path[i], Path.ChangeExtension(files_path[i], ".twb"));
+
+            }
+            else if (fi.Extension == ".twbx")
+            {
+
+                File.Move(files_path[i], Path.ChangeExtension(files_path[i], ".zip"));
+
+                files_path[i] = Path.ChangeExtension(files_path[i], ".zip");
+
+                string file_twb = files_path[i].Replace("zip", "twb");
+                int index = files_path[i].LastIndexOf(@"\");
+                index++;
+                file_twb = file_twb.Substring(index, file_twb.Length - index);
+
+                string file_twb_aux;
+                string path = files_path[i].Substring(0, index);
+                using (ZipFile zip = ZipFile.Read(files_path[i]))
+                {
+                    ZipEntry file = new ZipEntry();
+
+                    foreach (ZipEntry entry in zip.Entries)
+                    {
+                        if (entry.FileName.Contains("twb"))
+                        {
+                            file = entry;
+                            file.Extract(path);
+                            break;
+                        }
+                    }
+
+                    zip.RemoveEntry(file);
+                    zip.Save();
+
+                    file_twb_aux = path + file.FileName;
+
+                    File.Move(file_twb_aux, Path.ChangeExtension(file_twb_aux, ".xml"));
+                    file_twb_aux = Path.ChangeExtension(file_twb_aux, ".xml");
+                }
+
+                XDocument doc;
+                try
+                {
+                    doc = XDocument.Load(file_twb_aux);
+                }
+                catch (XmlException xmle)
+                {
+                    MessageBox.Show("Erro ao Carregar o Arquivo!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show(xmle.ToString());
+                    return;
+                }
+
+                var elements = doc.Descendants("connection");
+
+                foreach (XElement element in elements)
+                {
+                    if ((string)element.Attribute("server") != null || element.Attribute("server").ToString() != "")
+                    {
+                        element.Attribute("server").Value = "tableau.scanntech.com";
+                    }
+                }
+
+                doc.Save(file_twb_aux);
+
+                File.Move(file_twb_aux, Path.ChangeExtension(file_twb_aux, ".twb"));
+                file_twb_aux = Path.ChangeExtension(file_twb_aux, ".twb");
+
+
+                using (ZipFile zip = ZipFile.Read(files_path[i]))
+                {
+                    zip.AddFile(file_twb_aux, "");
+                    zip.Save();
+                }
+
+                File.Delete(file_twb_aux);
+                File.Move(files_path[i], Path.ChangeExtension(files_path[i], ".twbx"));
+            }
+
+            backgroundWorker1.ReportProgress(i);
+            Thread.Sleep(500);
+            e.Result = true;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            int total = e.ProgressPercentage + 1;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+                MessageBox.Show("Operação Cancelada", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                _count++;
+                lbl_pct.Text = _count + "/" + _TotalArquivos;
+                materialProgressBar1.Value = _count;
+
+                if (_count == _TotalArquivos)
+                {
+                    MessageBox.Show("Servidor Alterado com Sucesso!", "Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    materialListView1.Items.Clear();
+                    lbl_pct.Text = "Concluído!";
+                    _count = 0;
+                }
+            }
+        }
+    }
+}
